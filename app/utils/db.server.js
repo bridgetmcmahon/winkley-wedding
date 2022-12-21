e38eba8fcf51088bd311b5156804981e6fd15ed6
bridget mcmahon <bridget@yarno.com.au>
2022-12-22 08:55:55 +1000

    parse key?

--- app/utils/db.server.js
+++ app/utils/db.server.js
@@ -20,7 +20,7 @@ if (!admin?.apps?.length) {
     credential: cert({
       projectId: process.env.GOOGLE_PROJECT_ID,
       clientEmail: process.env.GOOGLE_CLIENT_EMAIL,
-      privateKey: process.env.GOOGLE_PRIVATE_KEY
+      privateKey: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, "\n")
     }),
     databaseURL: "https://winkley-wedding-rsvps.firebaseio.com"
   })
